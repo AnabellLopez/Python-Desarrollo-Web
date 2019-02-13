1bb27b8cab47e8862b9d790176a5b380e2e32a2b
AnabellLopez <47478828+AnabellLopez@users.noreply.github.com>
2019-02-13 15:24:48 -0500

    Update 6.py

--- 6.py
+++ 6.py
@@ -4,3 +4,13 @@ S = input()
 L = list(map(int, S.split(" ")))
 my_array = numpy.array(L)
 print(numpy.reshape(my_array,(3,3)))
+#You are given a X integer array matrix with space separated elements (N = rows and M = columns).Your task is to print the transpose and flatten results.
+import numpy
+N, M = input().split(" ")
+L = []
+for i in range(int(N)):
+    S = list(map(int, input().split(" ")))
+    L.append(list(S))
+my_array = numpy.array(L)
+print(numpy.transpose(my_array))
+print(my_array.flatten())
