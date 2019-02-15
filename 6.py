7b41ad5cf12f668817f4a7c7fee173b5aa4d2ea1
AnabellLopez <47478828+AnabellLopez@users.noreply.github.com>
2019-02-15 18:30:16 -0500

    Update 6.py

--- 6.py
+++ 6.py
@@ -68,3 +68,14 @@ A = list(map(float, input().split(" ")))
 print(numpy.floor(A))
 print(numpy.ceil(A))
 print(numpy.rint(A))
+
+#You are given a 2-D array with dimensions NxM. Your task is to perform the sum tool over axis 0 and then find the product of that result.
+import numpy
+
+N, M = input().split(" ")
+A = []
+for i in range(int(N)):
+    S = list(map(int, input().split(" ")))
+    A.append(S)
+B = numpy.sum(A, axis = 0)
+print(numpy.prod(B))
